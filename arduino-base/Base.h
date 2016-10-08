69170904f62c5782254f3689d995d9196f1fb42a
kastrulya <julia.hrihorieva@gmail.com>
2016-10-08 12:28:36 +0300

    added implementation for clap blink

--- arduino-base/Base.h
+++ arduino-base/Base.h
@@ -1,54 +1,75 @@
 //
 // Created by dzidzoiev on 9/25/16.
 //
-#include <LinkedList/LinkedList.h>
-#include <functional-vlpp/src/functional-vlpp.h>
+//#include <LinkedList/LinkedList.h>
+//#include <functional-vlpp/src/functional-vlpp.h>
 #include <Arduino.h>
 
 #ifndef CURTAINS_OPENER_BASE_H
 #define CURTAINS_OPENER_BASE_H
 
-typedef vl::Func<void(void)> Callback;
+#ifndef COMPONENTS_COUNT
+#define COMPONENTS_COUNT 5
+#endif
 
-template<typename T>
-class ReactiveList : public LinkedList<T> {
-public:
-    void forEach(vl::Func<void(T element)> foreachFun) {
-        for (int i = 0; i < this->_size; i++)
-            foreachFun(this->get(i));
-    }
-};
+typedef void(*Callback)(void);
+
+//typedef vl::Func<void(void)> Callback;
+
+//template<typename T>
+//class ReactiveList : public LinkedList<T> {
+//public:
+//    void forEach(vl::Func<void(T element)> foreachFun) {
+//        for (int i = 0; i < this->_size; i++)
+//            foreachFun(this->get(i));
+//    }
+//};
 
 class Updatable {
 public:
     virtual void update() = 0;
 };
 
-static ReactiveList<Updatable *> updatable;
-
 namespace Component {
+
+    static Updatable** updatable = new Updatable*[COMPONENTS_COUNT];
+    static uint8_t count;
+
     static void registerComponent(Updatable *updatable1);
 
     static void updateAll();
 
     void registerComponent(Updatable *updatable1) {
-        updatable.add(updatable1);
+        updatable[count++] = updatable1;
     }
 
     void updateAll() {
-        updatable.forEach([](Updatable *component) -> void {
-            component->update();
-        });
+        for (int i = 0; i < count; ++i) {
+            if (updatable[i] != NULL)
+                updatable[i]->update();
+        }
     }
 }
 
 
-#define EVENT_NUM 1
+#ifndef EBUS_ENABLED
+#define EBUS_ENABLED 1
+#endif
+#if EBUS_ENABLED
+#ifndef CALLBACK_NUM
+#define CALLBACK_NUM 1
+#endif
+#ifndef EBUS_EVENT_NUM
+#define EBUS_EVENT_NUM 1
+#endif
+#ifndef EBUS_CALLBACK_NUM
+#define EBUS_CALLBACK_NUM 1
+#endif
 /**
  * Should be created as static field
  */
 class EventBus {
-    Callback listenersMap[EVENT_NUM][3];
+    Callback listenersMap[EBUS_EVENT_NUM][EBUS_CALLBACK_NUM];
 
 public:
 
@@ -67,6 +88,7 @@ public:
 };
 
 static EventBus *eventBus;
+#endif //EBUS_ENABLED
 
 class Timer : public Updatable {
     unsigned long fireTime;
--- clap-blinker/Main.cpp
+++ clap-blinker/Main.cpp
@@ -1,6 +1,8 @@
+#define EBUS_ENABLED 0
 #include <Arduino.h>
-#include <Base.h>
 
+#include <Base.h>
+#include <OneButton.h>
 enum State {
     T_ON,
     S_ON,
@@ -8,15 +10,20 @@ enum State {
     S_OFF,
 };
 
+static const unsigned int threshold = 40;
+
+void onClap();
+
 class Clap : public Updatable {
-    const unsigned int threshold = 20;
+    static const unsigned long debounce = 30;
     const uint8_t sensorPin;
-    boolean sound;
+    boolean wasSound;
+    unsigned long ts;
 
     bool isSound() const {
         auto soundValue = analogRead(sensorPin);
-        if (soundValue < 1000)
-            Serial.println(soundValue);
+//        if (soundValue < 1000)
+//            Serial.println(soundValue);
         return soundValue < threshold;
     }
 
@@ -26,47 +33,62 @@ public:
     }
 
     virtual void update() override {
-        if (isSound()) {
-            sound = true;
+//        Serial.println("update");
+        if (!wasSound && isSound()) {
+            wasSound = true;
+            ts = millis();
             return;
         }
+        if (ts + debounce > millis() && wasSound && !isSound()) {
+            wasSound = false;
 
-        if (sound && !isSound())
-            eventBus->fire(0);
+            onClap();
+        }
     }
 
 };
 
+static int isSound(uint8_t pin) {
+    auto soundValue = analogRead(pin);
+//        if (soundValue < 1000)
+//            Serial.println(soundValue);
+    return soundValue < threshold;
+}
 
 /****
  * PROGRAM
  ****/
 
-static State state = T_ON;
-static DigitalPin lamp(13);
+static State state = T_OFF;
+static DigitalPin lamp(12);
+static OneButton button(0, true, isSound);
 
-static Clap clap(0);
+//static volatile Clap clap(0);
 
 void onClap() {
     switch (state) {
         case T_ON:
-            lamp.on();
+            lamp.off();
             state = T_OFF;
             break;
         case T_OFF:
-            lamp.off();
+            lamp.on();
             state = T_ON;
             break;
     }
 }
 
 void setup() {
-    Serial.begin(9600);
-    lamp.on();
+    callbackFunction c;
+    button.attachDoubleClick(onClap);
+    onClap();
+//    Serial.begin(9600);
+//    lamp.on();
 //    eventBus->subscribe(0, vl::Func<void(void)>(onClap));
 }
 
 void loop() {
-    Serial.println(analogRead(0));
+    button.tick();
+//    Serial.println(analogRead(0));
     Component::updateAll();
 }
