8a291b358b58a5d984b67d7e5f0a73ff2bbcb613
dzidzoiev <artur.dzidzoiev@gmail.com>
2016-10-12 23:29:05 +0300

    Code simplified, 0 dependecies

--- clap-blinker/Main.cpp
+++ clap-blinker/Main.cpp
@@ -1,94 +1,66 @@
-#define EBUS_ENABLED 0
 #include <Arduino.h>
 
-#include <Base.h>
-#include <OneButton.h>
-enum State {
-    T_ON,
-    S_ON,
-    T_OFF,
-    S_OFF,
-};
+#define threshold 230
+#define DEBOUNCE_TIME 100
+#define SECOND_CLAP_DELAY 400
 
-static const unsigned int threshold = 40;
+static const uint8_t sensor = A0;
+static const uint8_t lamp = 8;
+
+uint8_t claps = 0;
+unsigned long tsInitial = 0;
+unsigned long ts = 0;
+boolean lightState = false;
 
 void onClap();
-
-class Clap : public Updatable {
-    static const unsigned long debounce = 30;
-    const uint8_t sensorPin;
-    boolean wasSound;
-    unsigned long ts;
-
-    bool isSound() const {
-        auto soundValue = analogRead(sensorPin);
-//        if (soundValue < 1000)
-//            Serial.println(soundValue);
-        return soundValue < threshold;
-    }
-
-public:
-    Clap(uint8_t analogPin) : sensorPin(analogPin) {
-        Component::registerComponent(this);
-    }
-
-    virtual void update() override {
-//        Serial.println("update");
-        if (!wasSound && isSound()) {
-            wasSound = true;
-            ts = millis();
-            return;
-        }
-        if (ts + debounce > millis() && wasSound && !isSound()) {
-            wasSound = false;
-
-            onClap();
-        }
-    }
-
-};
-
-static int isSound(uint8_t pin) {
-    auto soundValue = analogRead(pin);
-//        if (soundValue < 1000)
-//            Serial.println(soundValue);
-    return soundValue < threshold;
-}
-
-/****
- * PROGRAM
- ****/
-
-static State state = T_OFF;
-static DigitalPin lamp(12);
-static OneButton button(0, true, isSound);
-
-//static volatile Clap clap(0);
-
-void onClap() {
-    switch (state) {
-        case T_ON:
-            lamp.off();
-            state = T_OFF;
-            break;
-        case T_OFF:
-            lamp.on();
-            state = T_ON;
-            break;
-    }
-}
+bool isSound();
 
 void setup() {
-    callbackFunction c;
-    button.attachDoubleClick(onClap);
-    onClap();
+    pinMode(lamp, OUTPUT);
+    digitalWrite(sensor, HIGH);
 //    Serial.begin(9600);
-//    lamp.on();
-//    eventBus->subscribe(0, vl::Func<void(void)>(onClap));
 }
 
 void loop() {
-    button.tick();
-//    Serial.println(analogRead(0));
-    Component::updateAll();
+
+    boolean sound = isSound();
+
+    if (sound)
+    {
+        if (claps == 0)
+        {
+            tsInitial = ts = millis();
+            claps++;
+        }
+        else if (claps > 0 && millis()-ts >= DEBOUNCE_TIME)
+        {
+            ts = millis();
+            claps++;
+        }
+    }
+
+    if (millis()-tsInitial >= SECOND_CLAP_DELAY)
+    {
+        if (claps == 2)
+        {
+            onClap();
+        }
+        claps = 0;
+    }
+}
+
+void onClap() {
+    Serial.println(claps);
+    if (lightState)
+        digitalWrite(lamp, LOW);
+    else
+        digitalWrite(lamp, HIGH);
+    lightState = !lightState;
+}
+
+bool isSound() {
+    auto soundValue = ( 1024 - analogRead(sensor) ) / 4;
+//    if (soundValue > 10)
+//        Serial.println(soundValue);
+    return soundValue > threshold;
 }
