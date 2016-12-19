a15ad6bf9e54909439168731e02955dedf768945
dzidzoiev <artur.dzidzoiev@gmail.com>
2016-12-19 20:53:39 +0200

    Config renamed

--- curtains-esp-cpp/src/WifiConfig.h
+++ curtains-esp-cpp/src/WifiConfig.h
@@ -3,7 +3,7 @@
 
 #define WLAN_SSID       ""
 #define WLAN_PASS       ""
-#define MQTT_SERVER      ""
-#define MQTT_PORT  1883
+#define MQTT_SERVER     ""
+#define MQTT_PORT       1883
 
 #endif //CURTAINS_ESP_CPP_CONFIG_H
