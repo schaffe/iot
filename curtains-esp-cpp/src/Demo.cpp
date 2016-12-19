a7e05261dd86e156b8711b666d7653ded7db90eb
dzidzoiev <artur.dzidzoiev@themidnightcoders.com>
2016-12-19 12:51:15 +0200

    Curtains cpp on platformio - initial drop

---
+++ curtains-esp-cpp/src/Demo.cpp
@@ -0,0 +1,86 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "Config.h"
+
+WiFiClient client;
+PubSubClient MQTT(client);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void callback(char* topic, byte* payload, unsigned int length);
+
+void setup() {
+    Serial.begin(115200);
+    delay(10);
+
+    WiFi.begin(WLAN_SSID, WLAN_PASS);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(F("."));
+    }
+    Serial.println(F(" WiFi connected."));
+
+    MQTT.setServer(MQTT_SERVER, MQTT_PORT);
+    MQTT.setCallback(callback);
+}
+
+void callback(char* topic, uint8_t* payload, unsigned int length) {
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+    for (int i = 0; i < length; i++) {
+        Serial.print((char)payload[i]);
+    }
+    Serial.println();
+
+    // Switch on the LED if an 1 was received as first character
+    if ((char)payload[0] == '1') {
+        digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+        // but actually the LED is on; this is because
+        // it is acive low on the ESP-01)
+    } else {
+        digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+    }
+
+}
+
+void reconnect() {
+    // Loop until we're reconnected
+    while (!MQTT.connected()) {
+        Serial.print("Attempting MQTT connection...");
+        // Attempt to connect
+        if (MQTT.connect("ESP8266Client", "fewfwwef", "")) {
+            Serial.println("connected");
+            // Once connected, publish an announcement...
+            MQTT.publish("outTopic", "hello world");
+            // ... and resubscribe
+            MQTT.subscribe("inTopic");
+        } else {
+            Serial.print("failed, rc=");
+            Serial.print(MQTT.state());
+            Serial.println(" try again in 5 seconds");
+            // Wait 5 seconds before retrying
+            delay(5000);
+        }
+    }
+}
+
+void loop() {
+
+    if (!MQTT.connected()) {
+        reconnect();
+    }
+    MQTT.loop();
+
+    long now = millis();
+    if (now - lastMsg > 2000) {
+        lastMsg = now;
+        ++value;
+        snprintf (msg, 75, "hello world #%ld", value);
+        Serial.print("Publish message: ");
+        Serial.println(msg);
+        MQTT.publish("outTopic", msg);
+    }
+}
